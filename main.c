7b859182404bd09908bbdb628ab02a23a47dc450
Ryan Peabody <rpeabody43@gmail.com>
2024-11-05 18:28:25 -0500

    Remove hsb to rgb pseudocode

--- main.c
+++ main.c
@@ -34,25 +34,6 @@ static struct {
     float size_ratio;
 } display;
 
-/*
-    x = c * (1 - abs((h / 60) % 2 - 1))
-    
-    if h < 60:
-        r, g, b = c, x, 0
-    elif h < 120:
-        r, g, b = x, c, 0
-    elif h < 180:
-        r, g, b = 0, c, x
-    elif h < 240:
-        r, g, b = 0, x, c
-    elif h < 300:
-        r, g, b = x, 0, c
-    else:
-        r, g, b = c, 0, x
-
-    # Convert to RGB 0-255 range
-    r, g, b = [int(color * 255) for color in (r, g, b)]
-*/
 void colors_init() {
     SDL_Color* colors = malloc(n * sizeof(SDL_Color));
     float inc = 360.0 / n;
